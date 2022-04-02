ac3180cd281f491f82e0254a35d9c4ec76cb8cd4
Mateus <mateushbsb@gmail.com>
2022-04-02 15:06:53 -0300

    IGNEWS - 005: Implementation nextauth using github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,29 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggerIn = true;
+  const [ session ] = useSession();
 
-  return isUserLoggerIn ? (
+  console.log(session);
+
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Mateus
+      { session.user.name }
       <FiX color="#737380" className={styles.colseIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
